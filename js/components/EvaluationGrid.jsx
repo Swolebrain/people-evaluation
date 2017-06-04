49f7765a8fcb1dc85f9ce06d62857e531b5eb58d
swolebrain <thecodingteacher@gmail.com>
2017-06-04 18:25:36 -0400

    fix: refactored the code for the overlay so it has its own component

--- js/components/EvaluationGrid.jsx
+++ js/components/EvaluationGrid.jsx
@@ -1,62 +1,10 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-const $ = require('jquery');
+import GridOverlay from './GridOverlay.jsx';
 
 class EvaluationGrid extends Component{
   constructor(props){
     super(props);
     this.state = {visible: false};
-  }
-  generateGrid =()=>{
-    var gridPositions = this.props.evals.reduce( (prev,ev) => {
-        var name = ev.name;
-        var scorecard = ev.scorecard
-          .filter((e,i)=>i<=3)
-          .map(sci=>Number(sci.score) )
-          .reduce( (p,c) => p+c );
-        var leadership = ev.scorecard
-          .filter((e,i)=>i>3)
-          .map(sci=>Number(sci.score) )
-          .reduce( (p,c) => p+c );
-
-        var coreVals = Math.round(Object.keys(ev.coreVals).reduce( (p,c) => p+ Number(ev.coreVals[c]) , 0)/
-                                        Object.keys(ev.coreVals).length-1);
-
-        // console.log(leadership);
-        scorecard = Math.round((scorecard + leadership)/5)-1;
-        console.log(scorecard);
-        return prev.concat({name, scorecard, coreVals});
-    }, []);
-    console.log(gridPositions);
-    //TODO: PREVENT THIS FROM INSTANTIATING EVERY TIME THE FUNCTION RUNS
-    var grid = [ ["", "", "", "", "", ""], ["", "", "", "", "", ""], ["", "", "", "", "", ""],
-          ["", "", "", "", "", ""], ["", "", "", "", "", ""], ["", "", "", "", "", ""] ];
-
-    gridPositions.forEach( (e, idx) => {
-      var col = e.scorecard;
-      if (col < 0) col = 0;
-      if (col > 5) col = 5;
-      var row = 5-e.coreVals;
-      if (row < 0) row = 0;
-      if (row > 5) row = 5;
-      grid[row][col] += e.name+'\n';
-    });
-    var rows = grid.map( function(row, index){
-      var cells = row.map( (e, i) => <td key={i}> {e} </td>);
-      return (
-        <tr key={index}>
-          {cells}
-        </tr>);
-      });
-    return (
-      <div>
-        <table>
-          <tbody>
-            {rows}
-          </tbody>
-        </table>
-      </div>
-    );
   }
   _hideOverlay=()=>{
     this.setState({visible: false});
@@ -67,12 +15,12 @@ class EvaluationGrid extends Component{
   }
   render(){
     var style = {marginBottom: "20px"};
-    var overlayStyle = {height: $(document).height()};
+    var overlayStyle = {height: window.innerHeight};
     var overlay = <div></div>;
     if (this.state.visible){
       overlay = (
       <div className='overlay' style={overlayStyle} onClick={this._hideOverlay}>
-        {this.generateGrid()}
+        <GridOverlay evals={this.props.evals} />
       </div>);
     }
     return (
---
+++ js/components/GridOverlay.jsx
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+
+const GridOverlay = props => {
+  var gridPositions = props.evals.reduce( (prev,ev) => {
+    //console.log(ev);
+    var name = ev.name;
+    var scorecard = ev.scorecard;
+    if (name == "" || !Array.isArray(scorecard) || scorecard.length === 0){
+      return prev;
+    }
+    scorecard = scorecard
+      .filter((e,i)=>i<=3)
+      .map(sci=>Number(sci.score) )
+      .reduce( (p,c) => p+c, 0 );
+    var leadership = ev.scorecard
+      .filter((e,i)=>i>3)
+      .map(sci=>Number(sci.score) )
+      .reduce( (p,c) => p+c, 0 );
+
+    var coreVals = Math.round(Object.keys(ev.coreVals).reduce( (p,c) => p+ Number(ev.coreVals[c]) , 0)/
+                                    Object.keys(ev.coreVals).length-1);
+
+    // console.log(leadership);
+    scorecard = Math.round((scorecard + leadership)/5)-1;
+    //console.log(scorecard);
+    return prev.concat({name, scorecard, coreVals});
+  }, []);
+  //console.log(gridPositions);
+  //TODO: PREVENT THIS FROM INSTANTIATING EVERY TIME THE FUNCTION RUNS
+  var grid = [ ["", "", "", "", "", ""], ["", "", "", "", "", ""], ["", "", "", "", "", ""],
+        ["", "", "", "", "", ""], ["", "", "", "", "", ""], ["", "", "", "", "", ""] ];
+
+  gridPositions.forEach( (e, idx) => {
+    var col = e.scorecard;
+    if (col < 0) col = 0;
+    if (col > 5) col = 5;
+    var row = 5-e.coreVals;
+    if (row < 0) row = 0;
+    if (row > 5) row = 5;
+    grid[row][col] += e.name+'\n';
+  });
+  var rows = grid.map( function(row, index){
+    var cells = row.map( (e, i) => <td key={i}> {e} </td>);
+    return (
+      <tr key={index}>
+        {cells}
+      </tr>);
+    });
+  return (
+    <div>
+      <table>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+
+export default GridOverlay;
